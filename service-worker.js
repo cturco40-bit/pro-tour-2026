542e0e572e0a58646466491f76086e69ffef248c
cturco40-bit <cturco40@gmail.com>
2026-04-15 14:13:29 -0400

    Soften dark mode to muted forest green palette

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'protour2026-v11';
+const CACHE_NAME = 'protour2026-v12';
 
 self.addEventListener('message', e => {
   if (e.data && e.data.type === 'SKIP_WAITING') self.skipWaiting();
