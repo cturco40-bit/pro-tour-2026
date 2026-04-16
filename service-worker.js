daf78f280577c46c5bebf81af41bf64ce58a13ba
cturco40-bit <cturco40@gmail.com>
2026-04-16 10:53:35 -0400

    Remove text-transform uppercase from all inputs

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'protour2026-v12';
+const CACHE_NAME = 'protour2026-v13';
 
 self.addEventListener('message', e => {
   if (e.data && e.data.type === 'SKIP_WAITING') self.skipWaiting();
